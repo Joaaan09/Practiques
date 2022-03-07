67848d3aa11263b674398e7d2e78f1eda4b0115f
Joan Coll <77456140+Joaaan09@users.noreply.github.com>
2022-03-07 08:52:38 +0100

    Add files via upload

---
+++ enc_s_i_apagat_pwm.ino
@@ -0,0 +1,24 @@
+int LED = 9;
+int velocitat = 10;
+int pwm;
+
+void setup() 
+{ 
+  pinMode(LED1, OUTPUT); 
+}
+
+void loop() 
+{
+  for(pwm = 0; pwm<256; pwm++)
+{
+  analogWrite(LED1, pwm);
+  delay(velocitat);
+}
+
+  for(pwm=255; pwm>-1; pwm--)
+{
+  analogWrite(LED1,pwm);
+  delay(velocitat);
+}
+
+}
